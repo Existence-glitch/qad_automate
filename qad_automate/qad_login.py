dcabe3e14ac1fba77bee915bf08b60960a5ed1d8
Existence-glitch <joaquintapia.inf@gmail.com>
2024-08-28 11:32:52 -0400

    First version 1.0

--- qad_automate/qad_login.py
+++ qad_automate/qad_login.py
@@ -1,58 +1,28 @@
-from utils import execute_commands, execute_command
+from utils import run_cmd, enter, space
 from config import CONFIG
-from commands import *
 
 def login_qad(session, qad_version):
     if qad_version in CONFIG:
         if qad_version.startswith('new'):
-            login_qad_new(session, qad_version)
+            login_qad_new(session, CONFIG[qad_version])
         elif qad_version.startswith('old'):
-            login_qad_old(session, qad_version)
+            login_qad_old(session, CONFIG[qad_version])
     else:
         print(f"Invalid QAD version: {qad_version}")
 
 def login_qad_new(session, config):
-    execute_command(session, 'qadmenu', wait_for="Please Enter Your Selection:")
-    execute_command(session, 'US', wait_for="User ID:")
-    execute_command(session, config['user'], wait_for="Password:")
-    execute_command(session, config['pass'])
+    s = session
+    run_cmd(s, 'qadmenu' + enter(), wait_for = "~$")
+    run_cmd(s, 'US' + enter(), wait_for = "Selection")
+    run_cmd(s, config['user'] + enter(), wait_for = "Enter data or press F4 to end.")
+    run_cmd(s, config['pass'] + enter())
+    run_cmd(s, enter())
+    run_cmd(s, space())
 
 def login_qad_old(session, config):
-    execute_command(session, config['qadmenu'], wait_for="User Name")
-    execute_command(session, config['username'], wait_for="Password")
-    execute_command(session, config['password'], wait_for="QAD Menu")
-    execute_command(session, '\n', wait_for="QAD Menu")  # Extra enter
-    execute_command(session, ' ', wait_for="QAD Menu")  # Space to clear any messages
-
-"""
-def login_qad_new(session, qad_version):
-    username = CONFIG[qad_version]['user']
-    password = CONFIG[qad_version]['pass']
-
-    login_commands = [
-        'qadmenu',  # Enter qadmenu terminal
-        enter(),
-        'US',       # Select language
-        enter()
-    ]
-
-    execute_commands(session, login_commands, 20)
-
-def login_qad_old(session, qad_version):
-    username = CONFIG[qad_version]['username']
-    password = CONFIG[qad_version]['password']
-    qadmenu = CONFIG[qad_version]['qadmenu']
-
-    login_commands = [
-        qadmenu,
-        username,
-        enter(),
-        password,
-        enter(),
-        enter(),
-        space(),
-    ]
-
-    execute_commands(session, login_commands, 1)
-
-"""
+    s = session
+    run_cmd(s, config['qadmenu'])
+    run_cmd(s, config['username'] + enter())
+    run_cmd(s, config['password'] + enter())
+    run_cmd(s, enter())
+    run_cmd(s, space())
--- qad_automate/qad_menus.py
+++ qad_automate/qad_menus.py
@@ -1,32 +1,69 @@
-from utils import send_command, execute_commands
-from commands import *
+from utils import run_cmd, enter
 
-# Gets menu number and selects the function that automatically enters that menu
-def select_menu(session, menu_number):
+def navigate_to_menu(session, menu_number):
+    """
+    Navigate to the specified menu number.
+    
+    :param session: Paramiko session object
+    :param menu_number: Menu number in the format "X.Y.Z"
+    """
     menu_parts = menu_number.split('.')
     for part in menu_parts:
-        send_command(session, part + '\r')
+        run_cmd(session, part + enter())
+    print(f"Successfully navigated to menu {menu_number}")
+
+def execute_menu_function(session, qad_version, menu_number, function_name):
+    """
+    Execute a specific function for the given menu number and QAD version.
     
-    # After selecting the menu, call the corresponding function
-    if menu_number in menu_logic:
-        menu_logic[menu_number](session)
-    else:
-        print(f"No specific logic defined for menu {menu_number}")
+    :param session: Paramiko session object
+    :param qad_version: QAD version ('new' or 'old')
+    :param menu_number: Menu number in the format "X.Y.Z"
+    :param function_name: Name of the function to execute
+    """
+    menu_functions = menu_functions_new if qad_version.startswith('new') else menu_functions_old
+    
+    if menu_number not in menu_functions:
+        print(f"No functions defined for menu {menu_number} in {qad_version} version")
+        return
+    
+    if function_name not in menu_functions[menu_number]:
+        print(f"Function {function_name} not found for menu {menu_number} in {qad_version} version")
+        return
+    
+    menu_functions[menu_number][function_name](session)
 
-# MENU CÓDIGOS GENERALIZADOS
-def menu_36_2_13(session):
-    commands = [
-        'TEST SCRIPT PYTHON 1',
-        enter(),
-        'TEST SCRIPT PYTHON 1',
-        enter(),
-        enter()
-    ]
-
-    execute_commands(session, commands)
-
-# Map menu numbers to corresponding functions
-menu_logic = {
-    '36.2.13': menu_36_2_13,
-    # Add other menus here
+def _36_2_13_test(s):
+    run_cmd(s, "TEST Nombre de campo" + enter())
+    run_cmd(s, "TEST Valor" + enter())
+    run_cmd(s, "TEST Comentario" + enter())
+    
+# Dictionary to store menu-specific functions for new QAD version
+menu_functions_new = {
+    "36.2.13": {
+        "test": lambda session: _36_2_13_test(session),
+    },
+    # Add more menu numbers and their functions for new version as needed
 }
+
+# Dictionary to store menu-specific functions for old QAD version
+menu_functions_old = {
+    "36.2.13": {
+        "test": lambda session: _36_2_13_test(session),
+    },
+    # Add more menu numbers and their functions for old version as needed
+}
+
+def list_menu_functions(qad_version, menu_number):
+    """
+    List available functions for the given menu number and QAD version.
+    
+    :param qad_version: QAD version ('new' or 'old')
+    :param menu_number: Menu number in the format "X.Y.Z"
+    :return: List of available function names
+    """
+    menu_functions = menu_functions_new if qad_version.startswith('new') else menu_functions_old
+    
+    if menu_number in menu_functions:
+        return list(menu_functions[menu_number].keys())
+    return []
